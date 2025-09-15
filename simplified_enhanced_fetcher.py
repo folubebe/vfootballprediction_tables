1e9a7a34613e3e57c99c5ab9a9b8d3069fa38670
folubebe <tokunborulz@gmail.com>
2025-09-15 10:35:49 +0100

    Update Selenium for headless mode

--- simplified_enhanced_fetcher.py
+++ simplified_enhanced_fetcher.py
@@ -83,7 +83,7 @@ class EnhancedDataFetcher:
         try:
             chrome_options = ChromeOptions()
             # Uncomment next line for headless mode
-            # chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
